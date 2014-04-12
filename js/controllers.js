1c67ef0794473a48f535c6310b1e94037c77000b
jotinha <jotinha@github>
2014-04-12 02:38:01 +0100

    map working with hack

--- js/controllers.js
+++ js/controllers.js
@@ -1,57 +1,57 @@
 angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function($scope,$ionicLoading) {
-  // $scope.map = {
-  //     center: {
-  //         latitude: 45,
-  //         longitude: -73
-  //     },
-  //     zoom: 8
-  // };
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(43.07493,-89.381388),
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"),
-        mapOptions);
-
-    // Stop the side bar from dragging when mousedown/tapdown on the map
-    google.maps.event.addDomListener(document.getElementById('map'), 'mousedown', function(e) {
-      e.preventDefault();
-      return false;
-    });
-
-    $scope.map = map;
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-  
-  $scope.centerOnMe = function() {
-    if(!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
+  $scope.map = {
+      center: {
+          latitude: 45,
+          longitude: -73
+      },
+      zoom: 8
   };
+  // function initialize() {
+  //   var mapOptions = {
+  //     center: new google.maps.LatLng(43.07493,-89.381388),
+  //     zoom: 16,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //   var map = new google.maps.Map(document.getElementById("map"),
+  //       mapOptions);
 
-  $scope.$on('viewState.viewEnter', function(e, d) {
-    console.log('View enter', e, d, $scope);
-    var mapEl = angular.element(document.getElementById('map'));
-    var iScope = mapEl.isolateScope();
-    var map = iScope.map;
-    google.maps.event.trigger(map, "resize");
-  });
+  //   // Stop the side bar from dragging when mousedown/tapdown on the map
+  //   google.maps.event.addDomListener(document.getElementById('map'), 'mousedown', function(e) {
+  //     e.preventDefault();
+  //     return false;
+  //   });
+
+  //   $scope.map = map;
+  // }
+  // google.maps.event.addDomListener(window, 'load', initialize);
+  
+  // $scope.centerOnMe = function() {
+  //   if(!$scope.map) {
+  //     return;
+  //   }
+
+  //   $scope.loading = $ionicLoading.show({
+  //     content: 'Getting current location...',
+  //     showBackdrop: false
+  //   });
+
+  //   navigator.geolocation.getCurrentPosition(function(pos) {
+  //     $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+  //     $scope.loading.hide();
+  //   }, function(error) {
+  //     alert('Unable to get location: ' + error.message);
+  //   });
+  // };
+
+  // $scope.$on('viewState.viewEnter', function(e, d) {
+  //   console.log('View enter', e, d, $scope);
+  //   var mapEl = angular.element(document.getElementById('map'));
+  //   var iScope = mapEl.isolateScope();
+  //   var map = iScope.map;
+  //   google.maps.event.trigger(map, "resize");
+  // });
 
 })
 
